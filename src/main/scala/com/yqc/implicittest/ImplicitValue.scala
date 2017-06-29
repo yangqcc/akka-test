543efabc7704da5c594f2d6eafd7d995dc25e462
yangqc <yangqc_ars@outlook.com>
2017-06-29 08:55:19 +0800

    scala隐式函数多个隐式参数

---
+++ src/main/scala/com/yqc/implicittest/ImplicitValue.scala
@@ -0,0 +1,25 @@
+package com.yqc.implicittest
+
+/**
+  * Created by yangqc on 2017/6/29.
+  */
+class ImplicitValue {
+
+  def showInteger(implicit value: Int) = println(value)
+}
+
+object ImplicitValue {
+
+  implicit val implicitValue: Int = 12
+
+  def apply(): ImplicitValue = new ImplicitValue()
+}
+
+object TestImplicitValue {
+
+  import com.yqc.implicittest.ImplicitValue._
+
+  def main(args: Array[String]): Unit = {
+    ImplicitValue().showInteger
+  }
+}

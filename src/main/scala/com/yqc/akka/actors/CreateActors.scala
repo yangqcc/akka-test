eb80ddb4d72e7cd735c2ddc411d7e1bd40a0e2de
yangqc <yangqc_ars@outlook.com>
2017-07-01 23:53:25 +0800

    创建actor

--- src/main/scala/com/yqc/akka/actors/CreateActors.scala
+++ src/main/scala/com/yqc/akka/actors/CreateActors.scala
@@ -36,9 +36,26 @@ object MyActor1 {
 
 }
 
+/**
+  * The call to actorOf returns an instance of ActorRef.
+  * This is a handle to the actor instance and the only way to interact(互动;相互作用;互相影响) with it.
+  *
+  * The ActorRef is immutable(不可变的) and has a one to one relationship with the Actor it represents
+  *
+  * The ActorRef is also serializable and network-aware. This means that you can serialize it,
+  * send it over the wire and use it on a remote host and it will still be representing the same Actor on the original node,
+  * across the network.(ActorRef 是可序列化的，并且可以通过网络传输，仍然代表之前的节点)
+  *
+  * Actors are automatically started asynchronously when created.(创建完成便异步开启)
+  */
 class MyActor1 extends Actor with ActorLogging {
 
   import MyActor1._
+
+  /**
+    * 创建子actor
+    */
+  val child = context.actorOf(Props[MyActor1], name = "myChild")
 
   override def receive: Receive = {
     case Greeting(greeter) => log.info(s"I was greeted by $greeter.")

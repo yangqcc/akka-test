93e2f4819dd078d0eef989f9c477df641fc401b9
yangqc <yangqc_ars@outlook.com>
2017-07-02 23:37:04 +0800

    自定义邮箱

--- src/main/scala/com/yqc/akka/mailbox/MyPrioMailbox.scala
+++ src/main/scala/com/yqc/akka/mailbox/MyPrioMailbox.scala
@@ -1,8 +1,10 @@
 package com.yqc.akka.mailbox
 
-import akka.actor.{ActorSystem, PoisonPill}
+import akka.actor.{Actor, ActorSystem, PoisonPill, Props}
 import akka.dispatch.{PriorityGenerator, UnboundedPriorityMailbox}
+import akka.event.{Logging, LoggingAdapter}
 import com.typesafe.config.Config
+import com.yqc.akka.MyActor
 
 /**
   * Created by yangqc on 2017/7/2.
@@ -18,3 +20,20 @@ class MyPrioMailbox(settings: ActorSystem.Settings, config: Config) extends Unbo
     case otherwise => 1
   }
 )
+
+class Logger extends Actor {
+
+  val log: LoggingAdapter = Logging(context.system, this)
+
+  override def receive: Receive = {
+    case x => log.info(x.toString)
+  }
+}
+
+object TestMailBox {
+
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("mailBox")
+    val myActory = system.actorOf(Props[MyActor].withMailbox("prio-mailbox"))
+  }
+}

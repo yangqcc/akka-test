a17f6b1a73108e405b9dbe24e0df0d62bb9cc3d0
yangqc <yangqc_ars@outlook.com>
2017-07-08 10:59:34 +0800

    Akka become和unbecome 用法

---
+++ src/main/scala/com/yqc/akka/become/Swapper.scala
@@ -0,0 +1,37 @@
+package com.yqc.akka.become
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+/**
+  * Created by yangqc on 2017/7/8.
+  *
+  * become和unbecome实现替换和还原消息处理器
+  */
+case object Swap
+
+class Swapper extends Actor {
+
+  import context._
+
+  override def receive: Receive = {
+    case Swap =>
+      println("Hi")
+      become({
+        case Swap =>
+          println("Ho")
+          unbecome()
+      }, discardOld = false) //push on top instead of replace
+  }
+}
+
+object SwapperApp extends App {
+  val system = ActorSystem("SwapperSystem")
+  val swap = system.actorOf(Props[Swapper], name = "swapper")
+  swap ! Swap
+  swap ! Swap
+  swap ! Swap
+  swap ! Swap
+  swap ! Swap
+  swap ! Swap
+  swap ! Swap
+}

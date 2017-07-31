5f717488df8e28e0cbc448302be536fbae6de203
yangqc <yangqc_ars@outlook.com>
2017-08-01 01:08:34 +0800

    actor内部异步执行阻塞调用

--- src/main/scala/com/yqc/akka/dispatcher/DispatcherTest2.scala
+++ src/main/scala/com/yqc/akka/dispatcher/DispatcherTest2.scala
@@ -1,6 +1,6 @@
 package com.yqc.akka.dispatcher
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -21,9 +21,19 @@ class BlockingFutureActor extends Actor {
 
   override def receive: Receive = {
     case i: Int => println(s"Calling blocking Future:${i}")
+      //异步执行阻塞调用，但是远远没有这么简单
       Future {
         Thread.sleep(5000)
         println(s"Blocking future finished ${i}")
       }
+      println("after future!")
+  }
+}
+
+object TestBlocking {
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("system")
+    val blockingActor: ActorRef = system.actorOf(Props[BlockingFutureActor], "blockingFutureActor")
+    blockingActor ! 21
   }
 }

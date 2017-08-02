2f239be01944921a20aeea82806eda32c8698401
yangqc <yangqc_ars@outlook.com>
2017-08-02 23:48:52 +0800

    scala 加载默认配置

--- src/main/scala/com/yqc/config/ConfigTest1.scala
+++ src/main/scala/com/yqc/config/ConfigTest1.scala
@@ -10,5 +10,7 @@ object ConfigTest1 {
   def main(args: Array[String]): Unit = {
     val config: Config = ConfigFactory.load("router.conf").getConfig("akka.actor.deployment")
     println(config.getConfig("/parent/router1").getInt("nr-of-instances"))
+    val defaultConfig: Config = ConfigFactory.load().getConfig("CustomMailBox")
+    println(defaultConfig.getConfig("custom-dispatcher").getString("mailbox-type"))
   }
 }

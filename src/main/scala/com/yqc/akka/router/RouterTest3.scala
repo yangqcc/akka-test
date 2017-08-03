c9e691f346ab5becb58e07be8a63924a2cb35448
yangqc <yangqc_ars@outlook.com>
2017-08-03 22:29:42 +0800

    路由

---
+++ src/main/scala/com/yqc/akka/router/RouterTest3.scala
@@ -0,0 +1,24 @@
+package com.yqc.akka.router
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.routing.FromConfig
+import com.typesafe.config.ConfigFactory
+
+/**
+  * Created by yangqc on 2017/8/3
+  */
+class RouterActor extends Actor {
+  val router1: ActorRef = context.actorOf(FromConfig.props(Props[ChildActor]), "router1")
+
+  override def receive: Receive = {
+    case _: String => router1 ! "to route"
+  }
+}
+
+object RouterTest3 {
+  def main(args: Array[String]): Unit = {
+    val system: ActorSystem = ActorSystem("system", ConfigFactory.load("router.conf"))
+    val routerActor: ActorRef = system.actorOf(Props[RouterActor])
+    routerActor ! "hello"
+  }
+}

aebf54b04f640be3d7362f599d498c4773eac16a
yangqc <yangqc_ars@outlook.com>
2017-10-11 23:08:28 +0800

    actor外使用future

---
+++ src/main/scala/com/yqc/akka/future/DirectlyUseFuture.scala
@@ -0,0 +1,21 @@
+package com.yqc.akka.future
+
+import java.util.concurrent.Executors
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+  * 直接使用future,不使用actor
+  */
+object DirectlyUseFuture {
+
+  private implicit val ec = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(2))
+
+  def main(args: Array[String]): Unit = {
+    val future = Future {
+      "Hello" + ",World"
+    }
+    future.foreach((x: String) => print(x + "!"))
+    future foreach print
+  }
+}

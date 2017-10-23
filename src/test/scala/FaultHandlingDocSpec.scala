29929bc6e0001a4702deb82b8b8e18f48a523da2
yangqc <yangqc_ars@outlook.com>
2017-10-23 22:06:08 +0800

    build.sbt添加测试依赖

--- src/test/scala/FaultHandlingDocSpec.scala
+++ src/test/scala/FaultHandlingDocSpec.scala
@@ -1,8 +1,9 @@
 package com.yqc.akka.faulttolerance
 
 import akka.actor.ActorSystem
-import akka.testkit.javadsl.TestKit
+import akka.testkit.{ImplicitSender, TestKit}
 import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 
 /**
   *
@@ -14,7 +15,9 @@ import com.typesafe.config.ConfigFactory
   *
   * Created by yangqc on 2017/7/30
   */
-class FaultHandlingDocSpec(_system: ActorSystem) extends TestKit(_system) {
+
+class FaultHandlingDocSpec(_system: ActorSystem) extends TestKit(_system)
+  with ImplicitSender with FlatSpecLike with Matchers with BeforeAndAfterAll {
 
   def this() = this(ActorSystem(
     "FaultHandlingDocSpec",
@@ -26,7 +29,11 @@ class FaultHandlingDocSpec(_system: ActorSystem) extends TestKit(_system) {
       }
       """)))
 
-  //  "A supervisor" must "apply the chosen strategy for its child" in {
-  // code here
-  //  }
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A supervisor" must "apply the chosen strategy for its child" in {
+    // code here
+  }
 }
